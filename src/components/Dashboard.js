4d57d2c16e034ecbf5cb4aef43527efca267bbaf
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-07-10 13:59:45 +0200

    updated and removed some useless data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,7 +17,7 @@ import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
@@ -26,8 +26,8 @@ function MadeWithLove() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Built with love by the '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI
+      <Link color="inherit" href="https://antimatter-studios.com/">
+        Antimatter Studios
       </Link>
       {' team.'}
     </Typography>
@@ -164,8 +164,6 @@ export default function Dashboard() {
         </div>
         <Divider />
         <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
--- src/components/listItems.js
+++ src/components/listItems.js
@@ -51,33 +51,9 @@ export const mainListItems = (
 
         <ListItem button>
             <ListItemIcon>
-            <LayersIcon />
+                <LayersIcon />
             </ListItemIcon>
-            <ListItemText primary="DyanmoDB" />
+            <ListItemText primary="DynamoDB" />
         </ListItem>
     </div>
-);
-
-export const secondaryListItems = (
-  <div>
-    <ListSubheader inset>Saved reports</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
-    </ListItem>
-  </div>
 );
