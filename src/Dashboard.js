06438ae5623c162669d89365e23658f592d952a4
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-07-10 15:04:36 +0200

    remove unwanted components, I can always find out how they built these things by looking at the original material ui example code
    Signed-off-by: Chris Thomas <chris.thomas@antimatter-studios.com>

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -18,9 +18,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { ListItems } from './components/ListItems';
-import Chart from './components/Chart';
-import Deposits from './components/Deposits';
-import Orders from './components/Orders';
 
 function MadeWithLove() {
   return (
@@ -170,21 +167,9 @@ export default function Dashboard() {
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
             {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
+            <Grid item xs={12} md={12} lg={12}>
               <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <Orders />
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>Recent Activity</Typography>
               </Paper>
             </Grid>
           </Grid>
